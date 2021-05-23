60ca8f6f13707cf0c43c658057ea907247754893
Per Stoffer Jensen <per.jensen.a@man-es.com>
2021-05-23 08:22:12 +0200

    added tests, modules and changed path

---
+++ src/mymod/calc2.py
@@ -0,0 +1,10 @@
+
+def fac(n):
+    if n < 0:
+        raise ValueError()
+    
+    res = 1
+    for i in range(1,n+1):
+        res *= i
+
+    return res
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,8 @@
 import calc
 import pytest
+from mymod import calc2
+
+
 
 def test_fib():
     assert [1, 2, 3, 5, 8, 13, 21, 34, 55, 89] == [e for e in calc.fib(10)]
@@ -14,3 +17,13 @@ def test_div():
 def test_div_zero():
     with pytest.raises(ZeroDivisionError):
         calc.div(1,0)
+
+def test_fac():
+    assert 120 == calc2.fac(5)
+
+def test_fac2():
+    assert 1 == calc2.fac(0)
+
+def test_fac3():
+    with pytest.raises(ValueError):
+        calc2.fac(-1)
